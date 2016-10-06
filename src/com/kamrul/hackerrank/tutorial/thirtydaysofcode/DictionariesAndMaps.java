97a3194baa2170f04ea9f827377b7340fdf69566
Mohammad Kamrul Hasan <mkhrussell@gmail.com>
2016-10-06 18:07:24 +0600

    Day 09

---
+++ src/com/kamrul/hackerrank/tutorial/thirtydaysofcode/DictionariesAndMaps.java
@@ -0,0 +1,33 @@
+package com.kamrul.hackerrank.tutorial.thirtydaysofcode;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class DictionariesAndMaps {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		HashMap<String, Integer> map = new HashMap<>();
+		
+        int n = in.nextInt();
+        for(int i = 0; i < n; i++) {
+            String name = in.next();
+            int phone = in.nextInt();
+            map.put(name, phone);
+        }
+        
+        while(in.hasNext()){
+            String s = in.next();
+            Integer phone = map.get(s);
+            if(phone == null) {
+            	System.out.println("Not found");
+            }else {
+            	System.out.println(s + "=" + phone);
+            }
+            
+        }
+        in.close();
+	}
+
+}
---
+++ src/com/kamrul/hackerrank/tutorial/thirtydaysofcode/Recursion.java
@@ -0,0 +1,20 @@
+package com.kamrul.hackerrank.tutorial.thirtydaysofcode;
+
+import java.util.Scanner;
+
+public class Recursion {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		System.out.println("" + factorial(n));
+		in.close();
+	}
+
+	private static int factorial(int n) {
+		if(n <= 0)
+			return 1;
+		
+		return n * factorial(n - 1);		
+	}
+}
